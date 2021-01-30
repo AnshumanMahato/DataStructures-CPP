aa03c0f82b352f04ef7622539b26840b68f8829b
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-01-30 22:06:36 +0530

    Ase hi

--- Array/LongestPalindromicString.cpp
+++ Array/LongestPalindromicString.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int pallindrome_substring(string str,int L,int R)
 {
+
     while (L >= 0 && R <= str.size())
     {
+
         if(str[L] != str[R])
             break;
         L--;
         R++;
+
     }
+    
     return R-L-1;
 }
 
