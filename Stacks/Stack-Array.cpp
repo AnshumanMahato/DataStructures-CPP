8b05d505947efef63bf0ac45cbcee94cab6a796d
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-02-26 12:04:21 +0530

    main() menu

--- Stacks/Stack-Array.cpp
+++ Stacks/Stack-Array.cpp
@@ -68,8 +68,7 @@ int Stack::get(int pos)
 
 void Stack::show()
 {
-    std::cout << std::endl;
-    if(underflow)
+    if(underflow())
         std::cout << "Underflow in Stack";
     else
     {
@@ -84,7 +83,62 @@ void Stack::show()
 int main()
 {
     Stack st;
-    st.show();
-    st.push(0);
-    st.show();
+    int n,value,pos;
+    do
+    {
+        std::cout << "\nStack Menu\n"
+                  << "\n1.Push Element"
+                  << "\n2.Pop Element"
+                  << "\n3.Show Stack Elements"
+                  << "\n4.Peek Stack"
+                  << "\n5.Get Element"
+                  << "\n6.Exit"
+                  << "\nEnter choice between 1-6";
+        std::cin >> n;
+        switch (n)
+        {
+        case 1:
+            std::cout << "Enter no.:";
+            std::cin >> value;
+            st.push(value);
+            break;
+        case 2:
+            value = st.pop();
+            if(value == INT32_MIN)
+                std::cout << "Underflow in stack.";
+            else
+                std::cout << "Popped Value:" << value;
+            break;
+        case 3:
+            std::cout << "Stack:";
+            st.show();
+            break;
+        case 4:
+            value = st.peek();
+            if(value == INT32_MIN)
+                std::cout << "Underflow in stack.";
+            else
+                std::cout << "Top Value:" << value;
+            break;
+        case 5:
+            std::cout << "Enter position value.:";
+            std::cin >> pos;
+            value = st.get(pos);
+            if(value == INT32_MIN)
+                std::cout << "Underflow in stack.";
+            else if (value == INT32_MAX)
+                std::cout << "Position not in range";
+            else
+                std::cout << "Value at " << pos <<':' << value;
+            break;
+        case 6:
+            std::cout << "Quitting...";
+            break;
+        default:
+            std::cout << "Invalid option";
+            break;
+        }
+    } while (n != 6);
+    
+    return 0;
 }
