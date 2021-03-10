d774b32f05aac882de5a4dfe40d81a3f6c631b48
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-03-10 14:57:20 +0530

    main

--- Queues/Queue-LL.cpp
+++ Queues/Queue-LL.cpp
@@ -104,3 +104,74 @@ void Queue::show()
         }
     }
 }  
+
+int main()
+{
+    Queue q;
+    int n,value,pos;
+    do
+    {
+        std::cout << "\nQueue Menu\n"
+                  << "\n1.Insert Element"
+                  << "\n2.Delete Element"
+                  << "\n3.Show Queue Elements"
+                  << "\n4.Peek Queue Front"
+                  << "\n5.Peek Queue Rear"
+                  << "\n6.Get Element"
+                  << "\n7.Exit"
+                  << "\nEnter choice between 1-7";
+        std::cin >> n;
+        switch (n)
+        {
+        case 1:
+            std::cout << "Enter no.:";
+            std::cin >> value;
+            q.enqueue(value);
+            break;
+        case 2:
+            value = q.dequeue();
+            if(value == INT32_MIN)
+                std::cout << "Underflow in stack.";
+            else
+                std::cout << "Deleted Value:" << value;
+            break;
+        case 3:
+            std::cout << "Queue:";
+            q.show();
+            break;
+        case 4:
+            value = q.getFront();
+            if(value == INT32_MIN)
+                std::cout << "Queue is Empty";
+            else
+                std::cout << "Front Value:" << value;
+            break;
+        case 5:
+            value = q.getRear();
+            if(value == INT32_MIN)
+                std::cout << "Queue is Empty";
+            else
+                std::cout << "Rear Value:" << value;
+            break;
+        case 6:
+            std::cout << "Enter position value.:";
+            std::cin >> pos;
+            value = q.get(pos);
+            if(value == INT32_MIN)
+                std::cout << "Queue is empty.";
+            else if (value == INT32_MAX)
+                std::cout << "Position not in range";
+            else
+                std::cout << "Value at " << pos <<':' << value;
+            break;
+        case 7:
+            std::cout << "Quitting...";
+            break;
+        default:
+            std::cout << "Invalid option";
+            break;
+        }
+    } while (n != 6);
+    
+    return 0;
+}
