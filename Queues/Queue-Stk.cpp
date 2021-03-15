3e26522adaaaef62988083eb32666ecff9a6267e
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-03-15 14:25:52 +0530

    class init

--- Queues/Queue-Stk.cpp
+++ Queues/Queue-Stk.cpp
@@ -1,11 +1,22 @@
 #include <bits/stdc++.h>
 #include "../Stacks/Stack-LL.cpp"
 
-int main()
+class Queue
 {
-    Stack st;
-    st.push(1);
-    st.push(2);
-    st.show();
-    return 0;
-}
+private:
+    Stack qu;
+    int len;
+    bool underflow()
+    {
+        return (qu.peek() == INT32_MIN);
+    }
+public:
+    Queue();
+    ~Queue();
+    void enqueue(int);
+    int dequeue();
+    int getFront();
+    int getRear();
+    int get(int);
+    void show();  
+};
