6ac4b8b3ec0e8b5e6812ea6462e6cd13eb1b7a5a
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-15 10:29:47 +0530

    Stock Price leet code problem trail 2 fail

--- Array/StockPriceLeetCode.cpp
+++ Array/StockPriceLeetCode.cpp
@@ -2,13 +2,13 @@
 
 int maxProfit(std::vector<int>& prices) {
         
-     int pos_lprice = 0,pos_hprice=prices.size();
+     int pos_lprice = 0,pos_hprice=-1;
         
     for(int i = 1; i<prices.size(); i++)
         {
-            if(prices[i] < prices[pos_lprice])
+            if(prices[i] < prices[pos_lprice] )
                 pos_lprice = i;
-            else if(pos_hprice == prices.size())
+            else if(pos_hprice == -1)
                 pos_hprice = i;
             else if(prices[pos_hprice] < prices[i])
                 pos_hprice = i;
@@ -22,7 +22,7 @@ int maxProfit(std::vector<int>& prices) {
 
 int main()
 {
-    std::vector<int> prices = {1,2};
+    std::vector<int> prices = {2,4,1};
 
     std::cout<<maxProfit(prices);
     return 0;
