1cff69ef84d6c156144c186d229d843048e358d4
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-23 10:02:19 +0530

    array leetcode problem

---
+++ Array/evenDigits-LeetCode.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int findNumbers(std::vector<int>& nums) {
+        
+        int count = 0;
+        
+        for(auto i = nums.begin(); i != nums.end(); i++)
+        {
+            int dig_count = 0;
+            while(*i)
+            {
+                *i /= 10;
+                dig_count++;
+            }
+            
+            if(dig_count%2 == 0)
+                count++;
+        }
+        
+        return count;
+        
+    }
+};
