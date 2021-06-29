fcab83525c37b676df70af21974ac7765f5ee06d
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-29 11:41:08 +0530

    Leet code array program solved

--- Array/mergeSortedArrays-LeetCode.cpp
+++ Array/mergeSortedArrays-LeetCode.cpp
@@ -13,26 +13,25 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
                 nums1.push_back(nums2[i++]);
             return;
         }
-        vector<int> first(nums1.begin(),nums1.begin()+m);
+        vector<int> nums1_copy(nums1.begin(),nums1.begin()+m);
         int i=0,j=0,ptr=0;
         while(i < m && j < n)
         {
-            if(nums1[i]<=nums2[j])
-                nums1[ptr++] = first[i++];
+            if(nums1_copy[i]<=nums2[j])
+                nums1[ptr++] = nums1_copy[i++];
             else
                 nums1[ptr++] = nums2[j++];
         }
         
         while(i<m)
         {
-            nums1[ptr++] = first[i++];
+            nums1[ptr++] = nums1_copy[i++];
         }
         
         while(j<n)
         {
             nums1[ptr++] = nums2[j++];
         }
-        
 }
 
 int main()
