35f51e9c78064db9b215f082261d8feacea4792b
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-30 11:51:29 +0530

    remove duplicates leetCode

---
+++ Array/removeDuplicate-LeetCode.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+        
+        int i = nums.size()-1;
+        int last = i;
+        while(i)
+        {
+            if(nums[i] == nums[i-1])
+            {
+                nums[i] = nums[last--];
+            }
+            i--;
+        }
+        
+        return 0;
+        
+}
+
+int main()
+{
+    vector<int> nums = {0,1,2,2,3,0,4,2}; // Input array
+    int val = 2; // Value to remove
+    vector<int> expectedNums = {0,1,4,0,3}; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+    int k = removeDuplicates(nums); // Calls your implementation
+
+    assert(k == expectedNums.size());
+    sort(nums.begin(),nums.begin()+k); // Sort the first k elements of nums
+    for (int i = 0; i < expectedNums.size(); i++) {
+        assert(nums[i] == expectedNums[i]);
+    }       
+        return 0;
+    }
